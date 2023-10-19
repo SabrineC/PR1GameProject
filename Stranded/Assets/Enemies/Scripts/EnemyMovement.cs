78725981bec03c7958c04fec98cd891476b98494
Sabrine <91069477+SabrineC@users.noreply.github.com>
2023-10-19 21:23:54 +0100

    Added Tribeleader to Scene

--- Stranded/Assets/Enemies/Scripts/EnemyMovement.cs
+++ Stranded/Assets/Enemies/Scripts/EnemyMovement.cs
@@ -4,27 +4,52 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private Animator animator;
-    private Transform player;
-    private float speed;
-    private float range;
+    public Animator anim;
+    public Transform target;
+    public Transform homePosition;
+    public float speed;
+    public float maxRange;
+    public float minRange;
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-        player = FindObjectOfType<PlayerMovement>().transform;
+        anim = GetComponent<Animator>();
+        target = FindObjectOfType<PlayerMovement>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        FollowPlayer();
+        if (Vector2.Distance(target.position.transform.position) <= maxRange && Vector2.Distance(target.position, transform.position)>= minRange)
+        {
+            FollowPlayer();
+        }
+        else if(Vector2.Distance(target.position, transform.position) >= maxRange)
+        {
+            GoHome();
+        }
 
     }
 
     public void FollowPlayer()
     {
-        animator.SetBool("isMoving", true);
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        anim.SetBool("isMoving", true);
+        anim.SetFloat("moveX", (target.position.x - transform.position.x));
+        anim.SetFloat("moveY", (target.position.y - transform.position.y));
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    public void GoHome()
+    {
+        anim.SetFloat("moveX", (homePosition.position.x - transform.position.x));
+        anim.SetFloat("moveY", (homePosition.position.y - transform.position.y));
+        transform.position = Vector2.MoveTowards(transform.position, homePosition, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, homePosition.position) == 0)
+        {
+            anim.SetBool("isMoving", false);
+        }
     }
 }
+
+
