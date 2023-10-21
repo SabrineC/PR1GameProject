67deb6046999289e894b01547f19801e9a2b7101
asadullahanfishi <anfishiasad@gmail.com>
2023-10-21 02:05:17 +0100

    edited background , and some debugging.
    arrr why are u reading this for?

--- Stranded/Assets/scripts/skip button.cs
+++ Stranded/Assets/scripts/skip button.cs
@@ -8,7 +8,7 @@ public class skipbutton : MonoBehaviour
     // Start is called before the first frame update
     public void Loadscene()
     {
-        SceneManager.LoadScene("select");
+        SceneManager.LoadScene(2);
     }
 
    
