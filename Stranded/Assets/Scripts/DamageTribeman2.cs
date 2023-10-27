37278d40989409aaae0c2dcfa449dcb353920c36
Sirine <91069476+SirineC@users.noreply.github.com>
2023-10-28 00:34:42 +0100

    created player damage to all individual tribespeople

---
+++ Stranded/Assets/Scripts/DamageTribeman2.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTribeman2 : MonoBehaviour
+{
+    public Animator animator;
+    public int damage;
+    public EntityAttributes entity_attributes;
+
+    private bool isAttacking = false;
+    private bool inCollider = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Attack();
+            isAttacking = true;
+        }
+
+        // If enemies are in range and attack input is pressed deals damage
+        if(inCollider && isAttacking)
+        {
+            entity_attributes.Damaged(damage);
+            isAttacking = false;
+        }
+    }
+    void Attack()
+    {
+        // Link animation to attack
+        animator.SetTrigger("Attack");
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col) 
+    {
+        if (col.gameObject.CompareTag("Tribeman2"))
+        {
+            inCollider = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Tribeman2"))
+        {
+            inCollider = false;
+        }
+    }
+}
+
