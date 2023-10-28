7e242bf876b32bbbe47a3f8f2b26dae2a5d6e836
Sirine <91069476+SirineC@users.noreply.github.com>
2023-10-28 18:37:51 +0100

    Added player damage to / health bar decrease to animals

---
+++ Stranded/Assets/Scripts/DamageChicken5.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageChicken5 : MonoBehaviour
+{
+    public Animator animator;
+    public int damage;
+    public EntityAttributes entity_attributes;
+
+    private bool isAttacking = false;
+    private bool inCollider = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Attack();
+            isAttacking = true;
+        }
+
+        // If enemies are in range and attack input is pressed deals damage
+        if(inCollider && isAttacking)
+        {
+            entity_attributes.Damaged(damage);
+            isAttacking = false;
+        }
+    }
+    void Attack()
+    {
+        // Link animation to attack
+        animator.SetTrigger("Attack");
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col) 
+    {
+        if (col.gameObject.CompareTag("Chicken5"))
+        {
+            inCollider = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Chicken5"))
+        {
+            inCollider = false;
+        }
+    }
+}
+
