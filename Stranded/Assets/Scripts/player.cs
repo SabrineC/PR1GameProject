bd5c083b07188b68bf66c88d4584001390567d04
asadullahanfishi <anfishiasad@gmail.com>
2023-11-01 18:17:56 +0000

    fixed the bug

--- Stranded/Assets/Scripts/player.cs
+++ Stranded/Assets/Scripts/player.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
-public class player : MonoBehaviour
+
+
+public class player : MonoBehaviour //if player is changed to Player the game will game.
 {
     public static player Instance;
 
     public PlayerAttributes playerHealth;
-    
 
     public void Awake()
     {
         player.Instance = this;
     }
      
-    public void IncreaseHealth(int value)
+     
+    public void IncreaseHealth(int value) //This code Increases The health of the player by the value.
     {
         playerHealth.IncreaseHealth(value);
         
     } 
        
 }
-
