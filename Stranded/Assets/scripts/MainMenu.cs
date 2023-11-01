b70cc2211a78ba4dfcfd863a35ff998803424dc1
asadullahanfishi <anfishiasad@gmail.com>
2023-11-01 15:49:47 +0000

    MainMenu Comments Added

--- Stranded/Assets/scripts/MainMenu.cs
+++ Stranded/Assets/scripts/MainMenu.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
-{
-    public void PlayGame()
+{   
+    public void PlayGame()     //This Code allows the 'Play' button to move to the correct scene once clicked.
     {
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 
-    public void QuitGame()
+    public void QuitGame()    //This Code allows for the user to leave the game once they click on the 'Exit' button.
     {
         Application.Quit();
     }
-
-
-
-
    
 }
