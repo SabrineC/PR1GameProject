6459ef334dffb6d0af62fbe9557d778c945bd28c
asadullahanfishi <anfishiasad@gmail.com>
2023-11-01 21:19:18 +0000

    fixed pause and text

--- Stranded/Assets/Scripts/SkipButton.cs
+++ Stranded/Assets/Scripts/SkipButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class skipbutton : MonoBehaviour
+public class SkipButton : MonoBehaviour
 {
     // Start is called before the first frame update
     public void Loadscene()  //This code allows the user to move directly to the game after clicking the 'skip' button.
